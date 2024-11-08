f6c246c56686091242a728f3779b01b2bf5b996a
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-11-08 17:29:50 +0300

    Добавляет скрипт запуска плеера

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,12 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const playerContainer = document.querySelector('.about-video-js');
+const playButton = playerContainer.querySelector('.about-video__control-js');
+const videoFrame = playerContainer.querySelector('iframe');
+
+playButton.addEventListener('click', () => {
+  playButton.style.display = 'none';
+  videoFrame.style.display = 'block';
+});
