330d4c733faa0c7af47a3d246a5ca5013506c92b
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-11-11 18:36:59 +0300

    Добавляет разметку и стилизацию блока Абонементы

--- source/js/main.js
+++ source/js/main.js
@@ -5,9 +5,12 @@
 
 const playerContainer = document.querySelector('.about-video-js');
 const playButton = playerContainer.querySelector('.about-video__control-js');
-const videoFrame = playerContainer.querySelector('iframe');
+const videoFrame = playerContainer.querySelector('.about-video__frame--js');
 
 playButton.addEventListener('click', () => {
+  const frameAttributes = videoFrame.getAttribute('allow').split('; ');
+  frameAttributes.push('autoplay');
+  videoFrame.setAttribute('allow', `${frameAttributes.join('; ')}`);
   playButton.style.display = 'none';
   videoFrame.style.display = 'block';
 });
