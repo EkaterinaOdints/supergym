f4d6e9c8f7730524064730f9e9300bec26ad554b
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2024-11-12 18:02:17 +0300

    Добавляет скрипт переключения табов

--- source/js/main.js
+++ source/js/main.js
@@ -3,14 +3,35 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
-const playerContainer = document.querySelector('.about-video-js');
-const playButton = playerContainer.querySelector('.about-video__control-js');
-const videoFrame = playerContainer.querySelector('.about-video__frame--js');
+const playVideo = () => {
+  const playerContainer = document.querySelector('.about-video-js');
+  const playButton = playerContainer.querySelector('.about-video__control-js');
+  const videoFrame = playerContainer.querySelector('.about-video__frame--js');
 
-playButton.addEventListener('click', () => {
-  const frameAttributes = videoFrame.getAttribute('allow').split('; ');
-  frameAttributes.push('autoplay');
-  videoFrame.setAttribute('allow', `${frameAttributes.join('; ')}`);
-  playButton.style.display = 'none';
-  videoFrame.style.display = 'block';
+  playButton.addEventListener('click', () => {
+    const frameAttributes = videoFrame.getAttribute('allow').split('; ');
+    frameAttributes.push('autoplay');
+    videoFrame.setAttribute('allow', `${frameAttributes.join('; ')}`);
+    playButton.style.display = 'none';
+    videoFrame.style.display = 'block';
+  });
+};
+
+playVideo();
+
+const tabContainer = document.querySelector('.prices-tab-js');
+const cardNameCollection = tabContainer.querySelectorAll('.tab-nav__item-js');
+const cardCollection = tabContainer.querySelectorAll('.prices-list-js');
+
+const changeTab = (index) => {
+  cardCollection.forEach((item) => {
+    item.classList.remove('prices-list--active');
+  });
+  const currentTab = cardCollection[index];
+  currentTab.classList.add('prices-list--active');
+};
+
+cardNameCollection.forEach((item, index) => {
+  const input = item.querySelector('.tab-nav__input-js');
+  input.addEventListener('change', () => changeTab(index));
 });
